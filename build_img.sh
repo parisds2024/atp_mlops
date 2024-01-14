76ddc1338e1c06ab42cf4bc27f2d3b440f9d1ecc
Ubuntu <ubuntu@ip-172-31-34-51.eu-west-1.compute.internal>
2024-01-14 19:00:58 +0000

    drop useless files

--- build_img.sh
+++
@@ -1,8 +0,0 @@
-echo "Image constructor"
-
-cd /src/data_generation
-docker image build . -t data_collection_image:latest
-cd ../src/training
-docker image build . -t model_training_image:latest
-cd ../src/app
-docker image build . -t api_image:latest
